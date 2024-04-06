0bda718ffd5bffb93c2c2b54404aade468e296f3
= <=>
2024-04-05 21:51:43 -0300

    feat: create model to enterprise and address

---
+++ src/domain/model/address.ts
@@ -0,0 +1,9 @@
+export interface Address {
+    id: number
+    street: string
+    number: string
+    district: string
+    city: string
+    state: string
+    cep: string
+}
---
+++ src/domain/model/enterprise.ts
@@ -0,0 +1,6 @@
+export interface Enterprise {
+    id:number
+    name: string
+    purpose: string
+    riNumber: string
+}
