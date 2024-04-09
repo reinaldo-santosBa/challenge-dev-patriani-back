afa313ffbc8516e1f76315690f053433f4f79301
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-04-08 23:03:50 -0300

    feat: added error in AppError

--- src/domain/repositories/addressRepositories.ts
+++ src/domain/repositories/addressRepositories.ts
@@ -9,9 +9,7 @@ export class AddressRepositories {
 			const newAddress = await dbConfig.address.create({ data: address });
 			return newAddress;
 		} catch (error) {
-			console.log(1);
-
-			throw new AppError('Internal server error', 500);
+			throw new AppError(error + '', 500);
 		}
 	}
 	async update(address: Address): Promise<Address> {
@@ -23,16 +21,19 @@ export class AddressRepositories {
 		if (!addressSearch) {
 			throw new AppError('Not content', 204);
 		}
-		const editAddress = await dbConfig.address.update(
-			{
-				where: {
-					id: address.id
-				},
-				data: address
-			}
-		);
-		return editAddress;
-
+		try {
+			const editAddress = await dbConfig.address.update(
+				{
+					where: {
+						id: address.id
+					},
+					data: address
+				}
+			);
+			return editAddress;
+		} catch (error) {
+			throw new AppError(error + '', 500);
+		}
 	}
 	async remove(id: number): Promise<null> {
 		try {
@@ -45,7 +46,7 @@ export class AddressRepositories {
 			);
 			return null;
 		} catch (error) {
-			throw new AppError('Internal server error', 500);
+			throw new AppError(error + '', 500);
 		}
 
 
@@ -55,7 +56,7 @@ export class AddressRepositories {
 			const allAddress = await dbConfig.address.findMany();
 			return allAddress;
 		} catch (error) {
-			throw new AppError('Internal server error', 500);
+			throw new AppError(error + '', 500);
 		}
 	}
 	async getById(id: number): Promise<Address> {
