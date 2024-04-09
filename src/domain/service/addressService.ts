9a7eea5024925c9d90d2f4fc1952041205ab469d
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-04-08 23:07:22 -0300

    refactor: remove trycatch

--- src/domain/service/addressService.ts
+++ src/domain/service/addressService.ts
@@ -1,17 +1,11 @@
-import { AppError } from '@errors';
 import { Address } from '@model';
 import { AddressRepositories } from 'domain/repositories/addressRepositories';
 
 export class AddressService {
-	async create(address: Address): Promise<Address> {
-		try {
-			const addressRepositories = new AddressRepositories();
-			const newAddress = await addressRepositories.create(address);
-			return newAddress;
-		} catch (error) {
-			console.log(error)
-			throw new AppError('Internal server erro', 500);
-		}
+	async create(address: Address): Promise<Address> {		
+		const addressRepositories = new AddressRepositories();
+		const newAddress = await addressRepositories.create(address);
+		return newAddress;
 	}
 
 	async update(address: Address): Promise<Address> {
@@ -21,31 +15,19 @@ export class AddressService {
 	}
 
 	async del(id: number): Promise<null> {
-		try {
-			const addressRepositories = new AddressRepositories();
-			const updateAddress = await addressRepositories.remove(id);
-			return updateAddress;
-		} catch (error) {
-			throw new AppError('Internal server erro', 500);
-		}
+		const addressRepositories = new AddressRepositories();
+		const updateAddress = await addressRepositories.remove(id);
+		return updateAddress;
 	}
 
 	async getAll(): Promise<Address[]> {
-		try {
-			const addressRepositories = new AddressRepositories();
-			const addressList = await addressRepositories.getAll();
-			return addressList;
-		} catch (error) {
-			throw new AppError('Internal server erro', 500);
-		}
+		const addressRepositories = new AddressRepositories();
+		const addressList = await addressRepositories.getAll();
+		return addressList;
 	}
 	async getById(id: number): Promise<Address> {
-		try {
-			const addressRepositories = new AddressRepositories();
-			const addressList = await addressRepositories.getById(id);
-			return addressList;
-		} catch (error) {
-			throw new AppError('Internal server erro', 500);
-		}
+		const addressRepositories = new AddressRepositories();
+		const addressList = await addressRepositories.getById(id);
+		return addressList;
 	}
 }
